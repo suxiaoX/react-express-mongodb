096757dc74908cdb3b67a86a8f6519e2cf3c0c21
suxiao <suxiao_cgc@163.com>
2017-07-26 21:05:05 +0800

    register UI

--- app/front/js/components/Register/index.js
+++ app/front/js/components/Register/index.js
@@ -3,7 +3,127 @@
  */
 import React from 'react';
 import '../../../scss/login.scss';
+import { Form, Input, Icon, Checkbox, Button } from 'antd';
+const FormItem = Form.Item;
+class RegistrationForm extends React.Component {
+  state = {
+    confirmDirty: false
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+  handleConfirmBlur = (e) => {
+    const value = e.target.value;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  }
+  checkPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && value !== form.getFieldValue('password')) {
+      callback('Two passwords that you enter is inconsistent!');
+    } else {
+      callback();
+    }
+  }
+  checkConfirm = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(['confirm'], { force: true });
+    }
+    callback();
+  }
 
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 6 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 14 }
+      }
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0
+        },
+        sm: {
+          span: 14,
+          offset: 6
+        }
+      }
+    };
+    return (
+      <Form onSubmit={this.handleSubmit} style={{width: '400px', margin: '80px auto'}}>
+        <FormItem
+          {...formItemLayout}
+          label="用户名"
+          hasFeedback
+        >
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: '请正确输入用户名' }]
+          })(
+            <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} />
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="密码"
+          hasFeedback
+        >
+          {getFieldDecorator('password', {
+            rules: [{
+              required: true, message: '请设置密码'
+            }, {
+              validator: this.checkConfirm
+            }]
+          })(
+            <Input type="password" prefix={<Icon type="lock" style={{ fontSize: 13 }} />} />
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="确认密码"
+          hasFeedback
+        >
+          {getFieldDecorator('confirm', {
+            rules: [{
+              required: true, message: '请确认密码'
+            }, {
+              validator: this.checkPassword
+            }]
+          })(
+            <Input type="password" onBlur={this.handleConfirmBlur} prefix={<Icon type="lock" style={{ fontSize: 13 }} />} />
+          )}
+        </FormItem>
+        <FormItem {...tailFormItemLayout} style={{ marginBottom: 8 }}>
+          {getFieldDecorator('agreement', {
+            valuePropName: 'checked'
+          })(
+            <Checkbox>我已阅读并同意<a href="">&nbsp;条款</a></Checkbox>
+          )}
+        </FormItem>
+        <FormItem {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit" className="login-form-button">注册</Button>
+          已有账号?<a href="/sign">&nbsp;去登录</a>
+        </FormItem>
+      </Form>
+    );
+  }
+}
+
+const Register = Form.create()(RegistrationForm);
+export default Register;
+/*
 export default class Register extends React.Component {
   render() {
     return (
@@ -29,3 +149,4 @@ export default class Register extends React.Component {
     );
   }
 }
+*/
