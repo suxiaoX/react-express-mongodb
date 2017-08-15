bc0f807a88eb9bd68c1c1e94181262a5e9e4f06f
suxiao <suxiao_cgc@163.com>
2017-08-15 12:02:13 +0800

    await

--- app/front/js/actions/articles.js
+++ app/front/js/actions/articles.js
@@ -2,7 +2,7 @@
  * @Author: leo 
  * @Date: 2017-08-14 16:53:31 
  * @Last Modified by: leo
- * @Last Modified time: 2017-08-15 11:58:45
+ * @Last Modified time: 2017-08-15 12:01:56
  */
 import { browserHistory } from 'react-router';
 import * as types from '../constants';
@@ -30,10 +30,13 @@ const fetchFailure = (error) => ({
 export const getArticles = (url) => async (dispatch) => {
     try {
         await dispatch(fetchRequset())
-        await get(url)
-            .then( response => {
-                dispatch(fetchSuccess(response.message));
-            }).cache(err => dispatch(fetchFailure(err)))
+        const data = await get(url);
+        console.log(data);
+        await dispatch(fetchSuccess(data.message));
+        // await get(url)
+        //     .then( response => {
+        //         dispatch(fetchSuccess(response.message));
+        //     }).cache(err => dispatch(fetchFailure(err)))
     } catch (err) {
         dispatch(fetchFailure(err));
     }
